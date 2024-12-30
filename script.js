b41a66a57c4e8e20f274e5829fb31f105deedd68
Gergelyi Márton <h045175@stud.u-szeged.hu>
2024-12-31 00:11:50 +0100

    loading screen

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 function goBack() {
     window.history.back();
 }
+
+
+// script.js
+window.addEventListener('load', () => {
+    const loadingScreen = document.getElementById('loading-screen');
+    const mainContent = document.getElementById('all-content');
+
+    setTimeout(() => {
+        // Blur és áttűnés a loading screen-nél
+        loadingScreen.style.opacity = '0';
+        loadingScreen.style.filter = 'blur(10px)';
+        loadingScreen.style.visibility = 'hidden';
+
+        // Fő tartalom megjelenítése
+        mainContent.style.display = 'flex';
+        setTimeout(() => {
+            mainContent.style.opacity = '1';
+        }, 50); // Kis késleltetés a sima megjelenéshez
+    }, 2000); // Legalább 2 másodpercig legyen látható a loading képernyő
+});
+
+
